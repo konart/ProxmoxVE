a87e513fa734fbd6f3126c62c3d7bab1ad1409eb
konart <366246+konart@users.noreply.github.com>
2026-09-07 18:45:20 +0300

    fileflows via filebin

--- ct/fileflows.sh
+++ ct/fileflows.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-_CS_DEFAULT_URL="https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main"
+_CS_DEFAULT_URL="https://raw.githubusercontent.com/konart/ProxmoxVE/libheif_github"
 _cs_boot="${COMMUNITY_SCRIPTS_CORE_DIR:-$(dirname "${BASH_SOURCE[0]}")/../../core}/core/build.func"
 source "$_cs_boot" 2>/dev/null || source <(curl -fsSL "${COMMUNITY_SCRIPTS_CORE_URL:-https://raw.githubusercontent.com/community-scripts/core/main}/core/build.func")
 # Copyright (c) 2021-2026 community-scripts ORG
@@ -103,7 +103,7 @@ function update_script() {
   fi
   msg_ok "Ensured ASP.NET Core Runtime"
 
-  fetch_and_deploy_from_url "https://fileflows.com/downloads/ff-latest.tar.xz" "/opt/fileflows"
+  fetch_and_deploy_from_url "https://filebin.net/k9kdh9yvcwmewse7/ff-latest.tar.xz" "/opt/fileflows"
 
   msg_info "Starting Service"
   systemctl --all start 'fileflows*'
